c534e0266d6e73d8a76a45c6e692b1af48bada4b
TheCoolMan13 <titust.ticusan@gmail.com>
2022-02-18 11:57:00 +0200

    master

---
+++ Scoala/C++/Laborator_18_Feb/Manin.cpp
@@ -0,0 +1,18 @@
+/*#include <iostream>
+#include "C:\Users\Titus Ticusan\.vscode\Vs code\Scoala\C++\AA - Main\Function.cpp"
+using namespace std;
+
+int main () {
+
+    int v[101] ;
+    int n , k , x , j;
+    Citire_Vector_Unidimensional(v, n);
+    Afisare_Vector_Unidimensional(v,n);
+    cin >> k >> x;
+    Inserare_In_Vector_Unidimensional_la_Punctul_k(v, n , k , x);
+    Afisare_Vector_Unidimensional(v,n);
+    cin >> j;
+    Stergere_In_Vector_Unidimensional_la_Punctul_k(v,n,j);
+    
+}
+*/
---
+++ Scoala/C++/Laborator_18_Feb/Problema_1.cpp
@@ -0,0 +1,14 @@
+//Se citeste un tablu cu n elemente nr intregi sa se stearga din tablou toate nr prime si apoi sa se
+//afiseze rezultatul
+/*int main () {
+    int v[101],n,i;
+    // citire(v,n) 
+    // afisare (v , n) 
+    for (int i = n ; i>=1;i--) {
+        if (prim(v[i])){
+            sterge (v,n,i);
+        }
+    }
+    afisare(v,n);
+}
+*/
