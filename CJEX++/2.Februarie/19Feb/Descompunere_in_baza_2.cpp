d40b03dfd19fdeddfe3849203d9788e0352b816d
TheCoolMan13 <titust.ticusan@gmail.com>
2022-02-19 11:14:08 +0200

    master

--- CJEX++/2.Februarie/19Feb/Descompunere_in_baza_2.cpp
+++ CJEX++/2.Februarie/19Feb/Descompunere_in_baza_2.cpp
@@ -3,7 +3,6 @@
 #include <math.h>
 #include <fstream>
 #include <climits>
-# include "C:\Users\Ticusan Titus\Desktop\Deale Mele\Vs code\New folder\Scoala\C++\AA - Main\Function.cpp"
 using namespace std;
 
 int Highest_Powerof_2_Less_Than_n(int n)
@@ -20,11 +19,11 @@ int Highest_Powerof_2_Less_Than_n(int n)
     return res;
 }
 
-void Descompunere_In_Puteri_Ale_Lui_2 (int k) {
+void Descompunere_In_Puteri_Ale_Lui_2(int k) {
     while (k != 0) {
         cout << Highest_Powerof_2_Less_Than_n(k);
         k = k - Highest_Powerof_2_Less_Than_n(k);
-        if (k!=0) cout << "+";
+        if (k!=0) cout << "+ ";
     }
 
 }
