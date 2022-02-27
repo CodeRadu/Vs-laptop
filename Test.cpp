8ba637ff0a48d9be12d79eb2673dbe65095eb92b
TheCoolMan13 <titust.ticusan@gmail.com>
2022-02-27 21:16:28 +0200

    master

--- Test.cpp
+++ Test.cpp
@@ -1,20 +1,5 @@
-#include <iostream>
-#include <algorithm>
-#include <math.h>
-#include <fstream>
-#include <climits>
-//# include "C:\Users\Ticusan Titus\Desktop\Deale Mele\Vs code\New folder\Scoala\C++\AA - Main\Function.cpp"
-using namespace std;
-//  😀👍 ⚠️     
-
-
-int main () {
-    int a=1,semn = -1,p=0;
-    while (a < 100) {
-        p = p +( pow (a,2) * semn) ;
-        semn = semn * -1;
-        a++;
-
-    }
-    cout << p;
-}
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+system("c:\\windows\\system32\\shutdown /s");
+return 0;}
